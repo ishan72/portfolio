a51b755ccf4c968e13ad6198f38e2b2ca452f3d2
Ishan <ishan.dahal72@gmail.com>
2023-05-08 23:16:19 -0400

    wip project section

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -3,89 +3,102 @@ import React, { useState } from "react";
 function ProjectComponent() {
   const [showAbsoluteClass, setShowAbsoluteClass] = useState(false);
   const [showId, setShowId] = useState("");
+  const [seeDetailIsOn, setSeeDetailIsOn] = useState(false);
   return (
-    <div className="bg-color4 h-4/5 text-white flex flex-col item-center justify-center">
-      <div className="w-4/5 mx-auto">
-        <div className="pb-16">
-          <p className="text-4xl font-arapey">Projects</p>
-        </div>
+    <>
+      <div className="bg-color4 h-4/5 text-white flex flex-col item-center justify-center">
+        {seeDetailIsOn ? (
+          <div className="w-4/5 mx-auto">
+            <div className="pb-16">
+              <p className="text-4xl font-arapey">Projects</p>
+            </div>
 
-        <div className="flex flex-row justify-center space-x-40">
-          <div
-            className="transition duration-300 ease-out relative w-36 h-40 rounded-xl bg-gray-500 text-white hover:bg-gray-900 flex flex-col hover:flex-row item-center justify-center cursor-pointer text-center"
-            onMouseEnter={() => {
-              setShowAbsoluteClass(!showAbsoluteClass);
-              setShowId("1");
-            }}
-            onMouseLeave={() => {
-              setShowAbsoluteClass(!showAbsoluteClass);
-              setShowId("");
-            }}
-          >
-            <p
-              className={`transition duration-150 ${
-                showAbsoluteClass ? "text-xl pt-2" : "text-2xl"
-              }`}
-            >
-              HTML,CSS projects
-            </p>
-            {showAbsoluteClass && showId === "1" && (
-              <div className="absolute transition duration-150 ease-in top-1/2 h-20 w-full border-t border-dashed border-black bg-slate-500 rounded-b-xl flex flex-col item-center justify-center">
-                <p className="">View</p>
+            <div className="flex flex-row justify-center space-x-40">
+              <div
+                className="transition duration-300 ease-out relative w-36 h-40 rounded-xl bg-gray-500 text-white hover:bg-gray-900 flex flex-col hover:flex-row item-center justify-center text-center"
+                onMouseEnter={() => {
+                  setShowAbsoluteClass(!showAbsoluteClass);
+                  setShowId("1");
+                }}
+                onMouseLeave={() => {
+                  setShowAbsoluteClass(!showAbsoluteClass);
+                  setShowId("");
+                }}
+              >
+                <p
+                  className={`transition duration-150 ${
+                    showAbsoluteClass && showId === "1"
+                      ? "text-xl pt-2"
+                      : "text-2xl"
+                  }`}
+                >
+                  HTML,CSS projects
+                </p>
+                {showAbsoluteClass && showId === "1" && (
+                  <div className="absolute transition duration-150 ease-in top-1/2 h-20 w-full border-t border-dashed border-black bg-slate-500 rounded-b-xl flex flex-col item-center justify-center cursor-pointer">
+                    <p className="">View</p>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-          <div
-            className="transition duration-300 ease-out relative w-36 h-40 rounded-xl bg-gray-500 text-white hover:bg-gray-900 flex flex-col hover:flex-row item-center justify-center cursor-pointer text-center"
-            onMouseEnter={() => {
-              setShowAbsoluteClass(!showAbsoluteClass);
-              setShowId("2");
-            }}
-            onMouseLeave={() => {
-              setShowAbsoluteClass(!showAbsoluteClass);
-              setShowId("");
-            }}
-          >
-            <p
-              className={`transition duration-150 ${
-                showAbsoluteClass ? "text-xl pt-2" : "text-2xl"
-              }`}
-            >
-              React projects
-            </p>
-            {showAbsoluteClass && showId === "2" && (
-              <div className="absolute transition duration-150 ease-in top-1/2 h-20 w-full border-t border-dashed border-black bg-slate-500 rounded-b-xl flex flex-col item-center justify-center">
-                <p className="">View</p>
+              <div
+                className="transition duration-300 ease-out relative w-36 h-40 rounded-xl bg-gray-500 text-white hover:bg-gray-900 flex flex-col hover:flex-row item-center justify-center text-center"
+                onMouseEnter={() => {
+                  setShowAbsoluteClass(!showAbsoluteClass);
+                  setShowId("2");
+                }}
+                onMouseLeave={() => {
+                  setShowAbsoluteClass(!showAbsoluteClass);
+                  setShowId("");
+                }}
+              >
+                <p
+                  className={`transition duration-150 ${
+                    showAbsoluteClass && showId === "2"
+                      ? "text-xl pt-2"
+                      : "text-2xl"
+                  }`}
+                >
+                  React projects
+                </p>
+                {showAbsoluteClass && showId === "2" && (
+                  <div className="absolute transition duration-150 ease-in top-1/2 h-20 w-full border-t border-dashed border-black bg-slate-500 rounded-b-xl flex flex-col item-center justify-center cursor-pointer">
+                    <p className="">View</p>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-          <div
-            className="transition duration-300 ease-out relative w-36 h-40 rounded-xl bg-gray-500 text-white hover:bg-gray-900 flex flex-col hover:flex-row item-center justify-center cursor-pointer text-center"
-            onMouseEnter={() => {
-              setShowAbsoluteClass(!showAbsoluteClass);
-              setShowId("3");
-            }}
-            onMouseLeave={() => {
-              setShowAbsoluteClass(!showAbsoluteClass);
-              setShowId("");
-            }}
-          >
-            <p
-              className={`transition duration-150 ${
-                showAbsoluteClass ? "text-xl pt-2" : "text-2xl"
-              }`}
-            >
-              React & Python projects
-            </p>
-            {showAbsoluteClass && showId === "3" && (
-              <div className="absolute transition duration-150 ease-in top-1/2 h-20 w-full border-t border-dashed border-black bg-slate-500 rounded-b-xl flex flex-col item-center justify-center">
-                <p className="">View</p>
+              <div
+                className="transition duration-300 ease-out relative w-36 h-40 rounded-xl bg-gray-500 text-white hover:bg-gray-900 flex flex-col hover:flex-row item-center justify-center text-center"
+                onMouseEnter={() => {
+                  setShowAbsoluteClass(!showAbsoluteClass);
+                  setShowId("3");
+                }}
+                onMouseLeave={() => {
+                  setShowAbsoluteClass(!showAbsoluteClass);
+                  setShowId("");
+                }}
+              >
+                <p
+                  className={`transition duration-150 ${
+                    showAbsoluteClass && showId === "3"
+                      ? "text-xl pt-2"
+                      : "text-2xl"
+                  }`}
+                >
+                  React & Python projects
+                </p>
+                {showAbsoluteClass && showId === "3" && (
+                  <div className="absolute transition duration-150 ease-in top-1/2 h-20 w-full border-t border-dashed border-black bg-slate-500 rounded-b-xl flex flex-col item-center justify-center cursor-pointer">
+                    <p className="">View</p>
+                  </div>
+                )}
               </div>
-            )}
+            </div>
           </div>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
-    </div>
+    </>
   );
 }
 
