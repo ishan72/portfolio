b089de7714decda55482d473459955ca434c94d3
Ishan <ishan.dahal72@gmail.com>
2024-11-19 22:01:56 -0600

    completed working with UI fix

--- src/UI/Container.jsx
+++ src/UI/Container.jsx
@@ -3,22 +3,18 @@ import React from "react";
 function Container({ index, title, children }) {
   return (
     <div
-      className={`min-h-screen ${
+      className={`min-h-screen h-full flex flex-col justify-center items-center space-y-20 ${
         index % 2 === 0
           ? "bg-gradient-to-bl from-white via-green-400 to-white"
           : "bg-gradient-to-tl from-white via-green-400 to-white"
       }`}
     >
-      <div className="min-h-screen h-full flex flex-col justify-center items-center">
-        <div>
+        <div className="">
           <p className="text-4xl italic">{title}</p>
         </div>
-        <div className="flex-1 bg-black h-full w-full">
           <div className="h-full flex justify-center items-center">
             {children}
           </div>
-        </div>
-      </div>
     </div>
   );
 }
---
+++ src/components/sections/ProjectSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../../UI/Container';
+
+function ProjectSection(){
+    return(
+        <Container index={3} title={"PROJECT"}>
+        <div className="">
+          <p>Ishan Dahal</p>
+        </div>
+      </Container>
+    )
+
+};
+
+export default ProjectSection;
